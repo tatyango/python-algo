215b15ddc0dd03d250acb06d8e5d9e69406d583b
tatyango <tatyango@users.noreply.github.com>
2025-06-15 17:05:44 +0200

    chore(int-to-roman): uncomment

--- integer-to-roman/my_romans.py
+++ integer-to-roman/my_romans.py
@@ -4,24 +4,19 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         """
         Convert int to roman
-
-        table = [
-                ... more to add
-                (10,   "X"), (9,    "IX"), (5,   "V"), (4,   "IV"),
-                (1,    "I"),
-                ]
         """
         val_map: List[tuple[int, str]] = [
-            (10,   "X"), (9,   "IX"), (5,   "V"), (4,   "IV"),
+            (10,   "X"),
+            (9,   "IX"),
+            (5,   "V"),
+            (4,   "IV"),
             (1,    "I"),
         ]
         res = []
-        #for val, sym in table:
-        #    count, num = divmod(num, val)
-        #    res.append(sym * count)
-        #return "".join(res)
-
-        raise NotImplementedError
+        for val, sym in val_map:
+            count, num = divmod(num, val)
+            res.append(sym * count)
+        return "".join(res)
 
     def romanToInt(self, s: str) -> int:
         """Convert roman numeral to int  (not ready)"""
