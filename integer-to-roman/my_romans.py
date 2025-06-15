7c6afb93221aedd59bbda83c20549b7836e89b9d
tatyango <tatyango@users.noreply.github.com>
2025-06-15 15:31:24 +0200

    feat(solution): solution skeleton with stubs

---
+++ integer-to-roman/my_romans.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        """Convert int to roman (not ready)"""
+        raise NotImplementedError
+
+    def romanToInt(self, s: str) -> int:
+        """Convert roman numeral to int  (not ready)"""
+        raise NotImplementedError
