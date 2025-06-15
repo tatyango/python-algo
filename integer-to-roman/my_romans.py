f7f2a585ae034160884955631d770b16666191ca
tatyango <tatyango@users.noreply.github.com>
2025-06-15 17:01:46 +0200

    feat(int-to-roman): add draft for greedy algo/loop for int to roman

--- integer-to-roman/my_romans.py
+++ integer-to-roman/my_romans.py
@@ -1,6 +1,26 @@
+from typing import List, Dict
+
 class Solution:
     def intToRoman(self, num: int) -> str:
-        """Convert int to roman (not ready)"""
+        """
+        Convert int to roman
+
+        table = [
+                ... more to add
+                (10,   "X"), (9,    "IX"), (5,   "V"), (4,   "IV"),
+                (1,    "I"),
+                ]
+        """
+        val_map: List[tuple[int, str]] = [
+            (10,   "X"), (9,   "IX"), (5,   "V"), (4,   "IV"),
+            (1,    "I"),
+        ]
+        res = []
+        #for val, sym in table:
+        #    count, num = divmod(num, val)
+        #    res.append(sym * count)
+        #return "".join(res)
+
         raise NotImplementedError
 
     def romanToInt(self, s: str) -> int:
